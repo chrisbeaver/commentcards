9f110a23b8efa66ed96ce3760e4b8cbd3b01fe7f
Chris Beaver <chris@chrisbeaver.net>
2023-03-21 15:57:24 -0400

    Initial commit.

---
+++ app/Models/CommentCard.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CommentCard extends Model
+{
+    use HasFactory;
+}
